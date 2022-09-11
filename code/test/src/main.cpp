26fd58a9b898ee870901123d67c7fc44fcc7e75a
chuancey <chuancey@isrc.com>
2022-09-11 15:55:07 +0800

    工厂设计模式

--- code/test/src/main.cpp
+++ code/test/src/main.cpp
@@ -2,28 +2,31 @@
 
 class Product {
 public:
-    virtual void create() {
+    virtual void info() = 0;
+    virtual ~Product() {
         
     }
 };
 
 class TypeAProduct : public Product {
 public:
-    virtual void create() {
-        std::cout << "create TypeA product" << std::endl;
-    }
-};  
-
-class TypeBProduct : public Product {
-public:
-    virtual void create() {
-        std::cout << "create TypeB product" << std::endl;
+    virtual void info() {
+        std::cout << "TypeA product info" << std::endl;
     }
 };
 
+class TypeBProduct : public Product {
+public:
+    virtual void info() {
+        std::cout << "TypeB product info" << std::endl;
+    }
+};
+
+// 简单工厂模式，根据描述信息获得产品
+namespace simple {
 class Factory {
 public:
-    static Product *createProduct(int type) {
+    static Product* createProduct(int type) {
         if (type == 0) {
             return new TypeAProduct;
         } else {
@@ -31,9 +34,44 @@ public:
         }
     }
 };
+}; // namespace simple
+
+// 工厂方法类,将工厂进一步细分，每种工厂只生产某种特定的产品
+namespace factoryFunction {
+class Factory {
+public:
+    virtual Product* createProduct() = 0;
+};
+
+class TypeAFactory : public Factory {
+public:
+    virtual Product* createProduct() override {
+        return new TypeAProduct;
+    }
+};
+
+class TypeBFactory : public Factory {
+public:
+    virtual Product* createProduct() override {
+        return new TypeBProduct;
+    }
+};
+}; // namespace factoryFunction
 
 int main() {
-    Product *product = Factory::createProduct(1);
-    delete product; 
+    // 简单工厂设计模式
+    {
+        Product* product = simple::Factory::createProduct(1);
+        delete product;
+    }
+
+    // 工厂方法设计模式
+    {
+        auto factory = new factoryFunction::TypeAFactory();
+        auto typeAProduct = factory->createProduct();
+        delete typeAProduct;
+        delete factory;
+    }
+
     return 0;
 }
