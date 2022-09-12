51db433a0872b65d7d266d2234f57a35f5791e78
chuancey <chuancey@isrc.com>
2022-09-12 15:14:47 +0800

    单例模式

--- code/test/src/main.cpp
+++ code/test/src/main.cpp
@@ -1,58 +1,104 @@
 #include <iostream>
 #include <list>
+#include <memory>
 
-namespace observer {
-
-// 抽象观察者，动作为更新状态、做出其他动作
-class Observer {
+namespace lazy {
+// 懒汉模式，只有在需要时才创建,程序结束时无法释放
+class SingleTon {
 public:
-    virtual void update() = 0;
-    virtual ~Observer() {
-    }
-};
-
-class Obj {
-public:
-    //对象在某种情况下会通知观察者
-    void notify() {
-        for (auto observer : observers) {
-            observer->update();
+    static SingleTon* getInstance() {
+        if (!instance) {
+            instance = new SingleTon();
         }
+        return instance;
     }
-
-    void attach(Observer* observer) {
-        observers.push_back(observer);
-    }
-    void detach(Observer* observer) {
-        observers.remove(observer);
-    }
-
-    ~Obj() {
-        for (auto observer : observers) {
-            delete observer;
-        }
+    void printHello() {
+        std::cout << "Hello lazy pattern!" << std::endl;
     }
 
 private:
-    // 对于该类对象，存在的观察者
-    std::list<Observer*> observers;
+    SingleTon() = default;
+    static SingleTon* instance;
 };
 
-class TypeObserver : public Observer {
+SingleTon* SingleTon::instance = nullptr;
+}; // namespace lazy
+
+// C++11 起的最佳写法
+namespace lazyAndThreadSafe {
+class Singleton {
 public:
-    virtual void update() override {
-        std::cout << "观察者A收到了信息" << std::endl;
+    static Singleton& getInstance() {
+        // C++11起静态局部变量线程安全
+        static Singleton instance;
+        return instance;
     }
-};
 
-}; // namespace observer
+public:
+    // 禁止拷贝和移动
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+    Singleton(Singleton&&) = delete;
+    Singleton& operator=(Singleton&&) = delete;
+
+public:
+    void printHello() {
+        std::cout << "Hello lazyAndThreadSafe pattern!" << std::endl;
+    }
+
+private:
+    Singleton() = default;
+    ~Singleton() = default;
+};
+}; // namespace lazyAndThreadSafe
+
+namespace smartpointer {
+class Singleton {
+public:
+    static Singleton& getInstance() {
+        // static unique_ptr
+        static std::unique_ptr<Singleton> instance(new Singleton);
+        return *instance;
+    }
+
+public:
+    void printHello() {
+        std::cout << "Hello smartpointer pattern!" << std::endl;
+    }
+
+public:
+    // 禁止拷贝和移动
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+    Singleton(Singleton&&) = delete;
+    Singleton& operator=(Singleton&&) = delete;
+    // 析构函数需要public权限，供智能指针调用析构函数
+    ~Singleton() = default;
+
+private:
+    Singleton() = default;
+
+private:
+    std::shared_ptr<Singleton> instance;
+};
+}; // namespace smartpointer
 
 int main() {
-    auto obser = new observer::TypeObserver();
-    auto obj = new observer::Obj();
-    obj->attach(obser);
-    obj->notify();
-    delete obj;
+    // 经典写法,存在内存泄漏
+    {
+        auto instance = lazy::SingleTon::getInstance();
+        instance->printHello();
+    }
 
+    // C++11 起的最佳写法
+    {
+        auto& instance = lazyAndThreadSafe::Singleton::getInstance();
+        instance.printHello();
+    }
+    // 利用智能指针但仍是静态局部变量
+    {
+        auto& instance = smartpointer::Singleton::getInstance();
+        instance.printHello();
+    }
     return 0;
 }
